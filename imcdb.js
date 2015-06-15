0681322a872746b90ba7274349ee6800634d1db3
hollanjs <jshollandsworth@gmail.com>
2015-06-15 09:38:25 -0400

    Updating readme.md file and imcdb.js file - no big updates, at least not big enough to mention with details.

--- imcdb.js
+++ imcdb.js
@@ -2,7 +2,10 @@
 var request = require('request'),
     cheerio = require('cheerio'),
 	fs = require('fs'),
-	movieLink = 'http://imcdb.org/movie.php?resultsStyle=asImages&sortBy=4&id=' + '415306',
+	// Update the ID below to the movie you want.
+	// See the readme for instructions on finding new movie IDs
+	movieID = '415306'
+	movieLink = 'http://imcdb.org/movie.php?resultsStyle=asImages&sortBy=4&id=' + movieID,
 	movieTitle = null,
     carTitle = [],
 	carSrc = [],
@@ -10,7 +13,7 @@ var request = require('request'),
 	numStar = [],
 	comp = [],
 	csvComp = [];
-    
+
 request (movieLink, function (err, resp, body) {
     if (!err && resp.statusCode ==200) {
         var $ = cheerio.load(body);
@@ -19,50 +22,50 @@ request (movieLink, function (err, resp, body) {
 			if (src.indexOf('res/star.png') == -1) {
 				carSrc.push(src);
 			}
-		});	
-		
+		});
+
 		$('div.ThumbnailBox','div.Gallery').each(function(){
 			var title = $(this).text();
 			carTitle.push(title);
 		});
-		
+
 		$('a.Thumbnail','div.Gallery').each(function(){
 			var link = 'http://imcdb.org/' + $(this).attr('href');
 			carLinks.push(link);
 		});
-		
+
 		$('h1.BoxTitle','#MovieVehicles').each(function(){
 			movieTitle = $(this).text();
 			movieTitle = movieTitle.replace(' Movie,','');
 		});
-		
+
 		$('span.Stars', 'div.ThumbnailBox').each (function () {
 			var star = $(this).first().children('img').length + 1;
 			numStar.push(star);
 		});
-		
+
 		for (i=0;i<carTitle.length;i++) {
 			comp.push(
-				"<div style='width: 50%; margin: 0 auto;'><a href='" + carLinks[i] + "' target='_blank'><img src='" + carSrc[i] + "' height='150px'></a><br />" + Array(numStar[i]).join('*')  + "<br /><a href='" + carLinks[i] + "' target='_blank'><span style='font-size:14px; vertical-align:bottom; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif'>" + carTitle[i] + "</span></a><br /></div><br /><br />" 
+				"<div style='width: 50%; margin: 0 auto;'><a href='" + carLinks[i] + "' target='_blank'><img src='" + carSrc[i] + "' height='150px'></a><br />" + Array(numStar[i]).join('*')  + "<br /><a href='" + carLinks[i] + "' target='_blank'><span style='font-size:14px; vertical-align:bottom; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif'>" + carTitle[i] + "</span></a><br /></div><br /><br />"
 			);
 		}
-		
+
 		for (i=0;i<carTitle.length;i++) {
 			csvComp.push(
 				 carTitle[i] + ',' + Array(numStar[i]).join('*') + '\n \t'
 			);
 		}
-			
+
         fs.writeFile('TalladegaNights.html',
 					 '<h1 style"font-family:Gotham, "Helvetica Neue", Helvetica, Arial, sans-serif">' + movieTitle + '</h1><br />' + comp.join(''),
 					 function(err){
-					 	console.log('html file successfully written!')	
+					 	console.log('html file successfully written!')
 					 });
-					 
+
 		fs.writeFile('TalladegaNights.csv',
 					 movieTitle + csvComp.join(''),
 					 function(err){
-					 	console.log('excel file successfully written!')	
+					 	console.log('excel file successfully written!')
 					 });
 		console.log('THE TOP 10 CARS FROM: ' + movieTitle);
 		console.log(csvComp.slice(0,10));
